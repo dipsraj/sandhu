4bdbbfe55beee58fb9026d781faedc016a52d700
Dipanjan Kundu <dipsraj.kundu@gmail.com>
2020-08-03 17:18:12 +0530

    Added User Quotation Mail template

--- modules/custom/sah_modifications/src/Controller/DefaultController.php
+++ modules/custom/sah_modifications/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\sah_modifications\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\Markup;
 use Drupal\webform\Entity\WebformSubmission;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -25,6 +26,7 @@ class DefaultController extends ControllerBase {
     }
     else {
       $price = \Drupal::config('sah_modifications.quote_price')->get('price');
+      $amt = $price * $pages;
       $values = [
         'webform_id' => 'webform_quote',
         'data' => [
@@ -33,12 +35,33 @@ class DefaultController extends ControllerBase {
           'subject' => $subject,
           'pages' => $pages,
           'amount_per_page' => $price,
-          'total_amount' => $price * $pages,
+          'total_amount' => $amt,
           'assignment_description' => $description,
           'deadline' => $deadline,
         ],
       ];
       WebformSubmission::create($values)->save();
+
+      $mailManager = \Drupal::service('plugin.manager.mail');
+      $module = 'sah_modifications';
+      $key = 'quote_notification_user';
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $send = true;
+
+      $params['data']['subject'] = $subject;
+      $params['data']['pages'] = $pages;
+      $params['data']['price'] = $price;
+      $params['data']['amt'] = $amt;
+
+      // Sending Quotation Email to the User
+      $to = $email;
+      $params['subject'] = 'Quotation Details at Sandhu Assignment Help';
+      $params['salutation'] = "Hello ".explode(' ',trim($name))[0].",";
+      $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+      if ($result)
+        \Drupal::messenger()->addMessage('Email Sent Successfully.');
+
+
       \Drupal::messenger()->addMessage('Thank You. Check your email for the Quotation.');
       $redirect = new RedirectResponse('/#get-quote');
       $redirect->send();
